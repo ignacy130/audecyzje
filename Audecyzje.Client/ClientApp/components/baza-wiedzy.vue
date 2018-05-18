c6337d4ba91abd0887d87693407c03e4549da307
felanilla <asia.karwan@gmail.com>
2018-05-19 01:12:46 +0200

    baza danych first row

--- Audecyzje.Client/ClientApp/components/baza-wiedzy.vue
+++ Audecyzje.Client/ClientApp/components/baza-wiedzy.vue
@@ -1,4 +1,23 @@
 <template>
+    <div class="subpage-view">
+        <div class="container">
+            <div class="row">
+                <div class="definition col-7">
+                    <h2>
+                        Czym jest reprywatyzacja
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, sint tritani pri id, pro veri delenit at, ius ex nibh quaestio. At sit nemore accusata urbanitas, te decore possit pri. Eu placerat neglegentur mei, vel id dicat ancillae. Eos postea prompta id.
+                    </p>
+                    <div class="quote">
+                        <img src="../img/quote.svg" alt="quote">
+                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Morbi scelerisque luctus velit. Praesent in mauris eu tortor porttitor accumsan. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. In enim a arcu imperdiet malesuada. Vivamus luctus egestas leo.</p>
+                    </div>
+                </div>
+                <div class="side-image col-5"></div>
+            </div>   
+        </div>
+    </div>
 </template>
 <script>
 export default {
@@ -10,4 +29,5 @@ export default {
     }
 </script>
 <style>
+
 </style>
