2b3c1c30b8a95ae6216b5dd8fd8c66189664e0dd
Ignacy <ignacy130@gmail.com>
2018-05-19 15:29:28 +0200

    add commas after routes.js merge

--- Audecyzje.Client/ClientApp/routes.js
+++ Audecyzje.Client/ClientApp/routes.js
@@ -7,8 +7,8 @@ import BazaWiedzy from 'components/baza-wiedzy'
 export const routes = [
     { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
     { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
-    { path: "*", component: PageNotFound }
-    { path: '/baza-wiedzy', component: BazaWiedzy, display: 'Baza wiedzy', style: 'glyphicon glyphicon-th-list' }
+    { path: "*", component: PageNotFound },
+    { path: '/baza-wiedzy', component: BazaWiedzy, display: 'Baza wiedzy', style: 'glyphicon glyphicon-th-list' },
     { path: '/map', component: Map, display: 'Mapa reprywatyzacji', style: 'glyphicon glyphicon-th-list' },
 ]
 
