f7f123da1918d973f0b3d83df97c8b67582750e7
Ignacy <ignacy130@gmail.com>
2018-05-19 17:33:48 +0200

    change hrefs to router-link at nav-menu

--- Audecyzje.Client/ClientApp/components/nav-menu.vue
+++ Audecyzje.Client/ClientApp/components/nav-menu.vue
@@ -1,27 +1,27 @@
 <template>
     <nav class="navbar navbar-expand-lg bg-dark" v-bind:class="{ 'navbar-dark': isHome }">
-        <a class="navbar-brand mx-auto" href="/">Społeczny Audyt Reprywatyzacji</a>
+        <router-link class="navbar-brand mx-auto" to="/">Społeczny Audyt Reprywatyzacji</router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-between" id="navbarText">
             <ul class="navbar-nav">
                 <li class="nav-item active mx-2">
-                    <a class="nav-link" href="/">
-                        Strona Główna
+                    <router-link class="nav-link" to="/">
+                    Strona główna
                         <span class="sr-only">(current)</span>
-                    </a>
+                    </router-link>
                 </li>
                 <li class="nav-item mx-2">
-                    <a class="nav-link" href="/mapa">Mapa Reprywatyzacji</a>
+                    <router-link class="nav-link" to="mapa">Mapa Reprywatyzacji</router-link>
                 </li>
             </ul>
             <ul class="navbar-nav">
                 <li class="nav-item mx-2">
-                    <a class="nav-link" href="baza-wiedzy">Baza Wiedzy</a>
+                    <router-link class="nav-link" to="baza-wiedzy">Baza Wiedzy</router-link>
                 </li>
                 <li class="nav-item mx-2">
-                    <a class="nav-link" href="kontakt">Kontakt</a>
+                    <router-link class="nav-link" to="kontakt">Kontakt</router-link>
                 </li>
             </ul>
         </div>
