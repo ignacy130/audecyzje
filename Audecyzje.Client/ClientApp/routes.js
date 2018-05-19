4e6c4f3ef3d1786f10ea3a293aa29fa213ac176d
Ignacy <ignacy130@gmail.com>
2018-05-19 15:25:02 +0200

    404

--- Audecyzje.Client/ClientApp/routes.js
+++ Audecyzje.Client/ClientApp/routes.js
@@ -1,9 +1,11 @@
 import CounterExample from 'components/counter-example'
 import Map from 'components/map-page'
 import HomePage from 'components/home-page'
+import PageNotFound from 'components/page-not-found'
 
 export const routes = [
     { path: '/', component: HomePage, display: 'Home', style: 'glyphicon glyphicon-home' },
     { path: '/counter', component: CounterExample, display: 'Counter', style: 'glyphicon glyphicon-education' },
-    { path: '/map', component: Map, display: 'Mapa reprywatyzacji', style: 'glyphicon glyphicon-th-list' }
+    { path: '/map', component: Map, display: 'Mapa reprywatyzacji', style: 'glyphicon glyphicon-th-list' },
+    { path: "*", component: PageNotFound }
 ]
